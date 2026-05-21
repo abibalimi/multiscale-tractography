c22029900d477cfd1d8c4ef85cc35a3a0ff4dd57
Olushola <abib.alimi@gmail.com>
2026-05-21 23:24:07 +0000

    Visualizing GRU encoder latent space using UMAP

---
+++ visualize_latent_space.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import umap.umap_ as umap
+
+from torch.utils.data import DataLoader
+from data.synthetic_streamlines import SyntheticTractographyDataset
+from lightning_modules.tractography_module import TractographyLightningModule
+
+from sklearn.cluster import KMeans
+
+
+def extract_latents(model, dataloader, device="mps"):
+    """
+    Extract latent representations from the trained model for visualization.
+    """
+    model.eval()
+
+    latent_vectors = []
+    labels = []
+
+    with torch.no_grad():
+
+        for batch in dataloader:
+
+            streamlines, y = batch
+            streamlines = streamlines.to(device)
+
+            reconstructed, z = model(streamlines)
+
+            latent_vectors.append(z.cpu().numpy())
+
+            labels.append(y.numpy())
+
+    latent_vectors = np.concatenate(latent_vectors, axis=0)
+    labels = np.concatenate(labels, axis=0)
+
+    return latent_vectors, labels
+
+
+def plot_streamlines(streamlines, title):
+    """
+    Visualize streamlines in 3D space.
+    """
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection="3d")
+
+    for streamline in streamlines:
+        ax.plot(
+            streamline[:, 0],
+            streamline[:, 1],
+            streamline[:, 2],
+            alpha=0.7
+        )
+
+    ax.set_title(title)
+
+    plt.show()    
+    
+    
+def main():
+
+    checkpoint_path = (
+        "lightning_logs/"
+        "version_4/"
+        "checkpoints/"
+        "epoch=9-step=1250.ckpt"
+    )
+
+    model = (
+        TractographyLightningModule
+        .load_from_checkpoint(
+            checkpoint_path
+        )
+    )
+
+    dataset = (
+        SyntheticTractographyDataset(
+            n_samples=2000
+        )
+    )
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=64,
+        shuffle=False
+    )
+
+    latent_vectors, labels = (
+        extract_latents(
+            model,
+            dataloader
+        )
+    )
+
+    reducer = umap.UMAP(
+        n_neighbors=50,
+        min_dist=0.3,
+        random_state=42
+    )
+
+    embedding = reducer.fit_transform(
+        latent_vectors
+    )
+    
+    tract_names = [
+        "Straight",
+        "Curved",
+        "Fan",
+        "Crossing"
+    ]
+
+    plt.figure(figsize=(10, 8))
+
+    for i in range(4):
+
+        idx = labels == i
+
+        plt.scatter(
+            embedding[idx, 0],
+            embedding[idx, 1],
+            label=tract_names[i],
+            alpha=0.7
+        )
+
+    plt.legend()
+
+    plt.title(
+        "Latent Space of "
+        "Synthetic Streamlines"
+    )
+
+    plt.xlabel("UMAP 1")
+    plt.ylabel("UMAP 2")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
