eda5cd9dc9a205859f4b17ff976162dc1f3cf541
Olushola <abib.alimi@gmail.com>
2026-05-22 16:06:35 +0000

    Moving streamlines visualization code to root of project

--- visualize_streamlines.py
+++ visualize_streamlines.py
@@ -4,7 +4,7 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-from synthetic_streamlines import SyntheticStreamlineGenerator
+from data.synthetic_streamlines import SyntheticStreamlineGenerator
 
 
 def main():
